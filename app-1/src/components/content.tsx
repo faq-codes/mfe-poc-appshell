0368d132d5615f9e1085ee0867bbc7d7331e911c
felipequiroz <felipe.quiroz@gmail.com>
2021-07-21 15:14:40 -0400

    version inicial

---
+++ app-1/src/components/content.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const content = () => (
+  <div style={{ border: '2px dashed green', height: '200px', width: '400px' }}>
+    CONTENT APP 1
+  </div>
+);
+
+export default content;
