0a0f581341b140dbe971522221963bfd4e3bc868
felipequiroz <felipe.quiroz@gmail.com>
2021-07-22 10:57:09 -0400

    intento carga de componente remoto

--- app-1/webpack.config.js
+++ app-1/webpack.config.js
@@ -44,10 +44,12 @@ module.exports = {
         ...deps,
         react: {
           singleton: true,
+          eager: true,
           requiredVersion: deps.react
         },
         'react-dom': {
           singleton: true,
+          eager: true,
           requiredVersion: deps['react-dom'],
         },
         'styled-components': {
